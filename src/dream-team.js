bbcc914c4b89edff3edf75a5781a671e0d24f035
VitaliiSavelyev <BinT0.5@mail.ru>
2021-03-08 20:55:11 +0300

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam( members ) {
+  if (!Array.isArray(members)){
+    return false
+  }
+  let firstLetters = [];
+  for (let member of members) {
+    if (typeof member === "string") {
+      let UpperLetter = member.toUpperCase();
+      if (member[0] === ' ') {
+        let z = UpperLetter.split('');
+        for (k = 0; k < z.length; k++) {
+          if (z[k] != ' ') {
+            firstLetters.push(z[k]);
+            break;
+          }
+        }
+      } else {
+        firstLetters.push(UpperLetter.charAt(0))
+      }
+    }
+  }
+  return firstLetters.sort().join('').toUpperCase();
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
