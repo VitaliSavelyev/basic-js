0d861ea446da88d34b1d83f2806f579d11078fe3
VitaliiSavelyev <BinT0.5@mail.ru>
2021-03-10 22:19:18 +0300

    refactor: delete old comented solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,41 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-/*
-const chainMaker = {
-    mass:[],
-  getLength() {
-      return this.mass.length;
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value='') {
-        console.log(this.mass);
-      this.mass.push(`( ${value} )`);
-      return this;
-   // throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-        if((position<1)||(position>this.mass.lengt)){throw new Error()}
-      this.mass.splice(position-1, 1);
-return this;
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-      this.mass.reverse();
-      return this;
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-        this.mass.join('~~');
-      return this.mass;
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
-*/
+/
 const chainMaker = {
     mass : [],
     getLength() {
